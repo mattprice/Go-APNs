993747c79afd812bd9988ff80f6f8a6da2296098
Matthew Price <hello@mattprice.me>
2013-07-02 23:59:43 -0400

    Send a -1 badge count to get around how omitempty works.

--- apns.go
+++ apns.go
@@ -15,6 +15,13 @@ type aps struct {
 }
 
 func (this *Notification) ToString() (string, error) {
+	// The omitempty option of `json.Marshal` considers "0" to be an empty value.
+	// Although it's not documented, you can send Apple "-1" instead of "0" in order
+	// to clear the badge icon, which saves us from needing to write our own omitempty function.
+	if this.Badge == 0 {
+		this.Badge = -1
+	}
+
 	alert := &aps{this}
 	bytes, err := json.MarshalIndent(alert, "", "  ")
 
