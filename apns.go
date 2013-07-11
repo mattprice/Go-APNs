116f579ec9f010ea794cf0a90596f36e430c1b70
Matthew Price <hello@mattprice.me>
2013-07-10 23:51:30 -0400

    Fix the localized notification style.
    I do not like this solution since it duplicates code. Still working
    on a better solution.

--- apns.go
+++ apns.go
@@ -16,8 +16,13 @@ type Notification struct {
 }
 
 type LocNotification struct {
-	Args []string
-	Notification
+	Args         []string
+	Alert        string
+	Badge        int
+	Sound        string
+	ActionLocKey string
+	LaunchImage  string
+	Custom
 }
 
 func NewNotification() *Notification {
@@ -33,6 +38,61 @@ func NewLocNotification() *LocNotification {
 	this.Custom = Custom{}
 
 	return this
+}
+
+func (this *LocNotification) ToString() (string, error) {
+	// I don't like going from Struct to Map to JSON, but this is the best solution
+	// I can come up with right now to continue keeping the API simple and elegant.
+	payload := make(map[string]interface{})
+	aps := make(map[string]interface{})
+
+	// If ActionLocKey or LaunchImage are set then we need to use the alert dictionary format.
+	if this.ActionLocKey != "" || this.LaunchImage != "" || len(this.Args) > 0 {
+		alert := make(map[string]interface{})
+
+		if this.Alert != "" {
+			alert["body"] = this.Alert
+		}
+
+		if this.ActionLocKey != "" {
+			alert["action-loc-key"] = this.ActionLocKey
+		}
+
+		if this.LaunchImage != "" {
+			alert["launch-image"] = this.LaunchImage
+		}
+
+		if len(this.Args) > 0 {
+			alert["loc-args"] = this.Args
+		}
+
+		aps["alert"] = &alert
+	} else if this.Alert != "" {
+		aps["alert"] = this.Alert
+	}
+
+	// The omitempty option of `json.Marshal` considers "0" to be an empty value.
+	// Although it's not documented, you can send Apple "-1" instead of "0" in order
+	// to clear the badge icon, which saves us from needing to write our own omitempty function.
+	aps["badge"] = this.Badge
+	if this.Badge == 0 {
+		aps["badge"] = -1
+	}
+
+	if this.Sound != "" {
+		aps["sound"] = this.Sound
+	}
+
+	// All standard dictionaries need to be wrapped in the "aps" namespace.
+	payload["aps"] = &aps
+
+	// Output all the custom dictionaries.
+	for key, value := range this.Custom {
+		payload[key] = value
+	}
+
+	bytes, err := json.MarshalIndent(payload, "", "  ")
+	return string(bytes), err
 }
 
 func (this *Notification) ToString() (string, error) {
--- examples/example.go
+++ examples/example.go
@@ -15,9 +15,13 @@ func main() {
 	// notificationStyleB()
 	// fmt.Println()
 
-	fmt.Println("Localized Notification, Style B:")
+	fmt.Println("Localized Notification, Style A:")
 	locNotifcationStyleB()
 	fmt.Println()
+
+	// fmt.Println("Localized Notification, Style B:")
+	// locNotifcationStyleB()
+	// fmt.Println()
 }
 
 func notificationStyleA() {
@@ -46,6 +50,25 @@ func notificationStyleB() {
 	payload.Sound = "Test.aif"
 	payload.LaunchImage = "Default.png"
 	payload.Custom["X-HTTP-Referer"] = "https://github.com/mattprice/Go-APNs"
+
+	// Output the payload as JSON, for testing.
+	str, _ := payload.ToString()
+	fmt.Println(str)
+}
+
+func locNotifcationStyleA() {
+	// Create a simple notification:
+	payload := &apns.LocNotification{
+		Alert:       "APP_GREETING",
+		Args:        []string{"John", "Smith"},
+		Badge:       42,
+		Sound:       "Test.aif",
+		LaunchImage: "Default.png",
+
+		Custom: apns.Custom{
+			"X-HTTP-Referer": "https://github.com/mattprice/Go-APNs/",
+		},
+	}
 
 	// Output the payload as JSON, for testing.
 	str, _ := payload.ToString()
