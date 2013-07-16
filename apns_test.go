8746f77508ff8f25824c6142e9945c81500cfa50
Matthew Price <hello@mattprice.me>
2013-07-15 23:33:35 -0400

    Revamp and finish creating the remaining unit tests.

--- apns_test.go
+++ apns_test.go
@@ -1,33 +1,70 @@
 package apns
 
 import (
-	"encoding/json"
 	"testing"
 )
 
-// TODO: This is all very inefficient. It's mostly meant as a proof of concept.
 func Test_Notification_ToJSON(t *testing.T) {
-	// Create a basic notification.
-	payload := &Notification{
-		Alert: "Hello, World! This is a test.",
-	}
+	// Test a basic notification.
+	payload := NewNotification()
+	payload.Alert = "Hello, World! This is a test."
 
-	// Turn the notification into JSON.
 	bytes, err := payload.ToJSON()
 	if err != nil {
-		t.Error(err)
+		t.Error("Unexpected error:", err)
 	}
 
-	// Turn the JSON back into a map.
-	output := make(map[string]interface{})
-	err = json.Unmarshal(bytes, &output)
+	if len(bytes) != 49 {
+		t.Errorf("Converting basic notification to JSON failed. Expected 49 bytes but found %v.", len(bytes))
+	}
+
+	// Test a complex notification.
+	payload = NewNotification()
+	payload.Alert = "Hello, World! This is a test."
+	payload.Badge = 42
+	payload.Sound = "Test.aiff"
+	payload.LaunchImage = "Default.png"
+	payload.Custom["Engines"] = Custom{
+		"DDG":  "https://duckduckgo.com/",
+		"Bing": "https://bing.com/",
+	}
+
+	bytes, err = payload.ToJSON()
 	if err != nil {
-		t.Error(err)
+		t.Error("Unexpected error:", err)
 	}
 
-	// Verify the data of the map.
-	aps := output["aps"].(map[string]interface{})
-	if aps["alert"] != payload.Alert {
-		t.Errorf("Output does not match: %#v != %#v", output["Alert"], payload.Alert)
+	if len(bytes) != 189 {
+		t.Errorf("Converting complex notification to JSON failed. Expected 189 bytes but found %v.", len(bytes))
+	}
+
+	// Test a localized notification.
+	// Conveniently, this also tests setting no badge count.
+	payload = NewNotification()
+	payload.Alert = "This should be ignored."
+	payload.AlertLocKey = "APP_GREETING_KEY"
+	payload.AlertLocArgs = []string{"John", "Smith"}
+
+	bytes, err = payload.ToJSON()
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+
+	if len(bytes) != 76 {
+		t.Errorf("Converting localized notification to JSON failed. Expected 76 bytes but found %v.", len(bytes))
+	}
+
+	// Test a badge count of 0.
+	// This was a problem in the past when using other `json.Marshal` methods.
+	payload = NewNotification()
+	payload.Badge = 0
+
+	bytes, err = payload.ToJSON()
+	if err != nil {
+		t.Error("Unexpected error:", err)
+	}
+
+	if len(bytes) != 19 {
+		t.Errorf("Alert with badge count of 0 failed. Expected 19 bytes but found %v.", len(bytes))
 	}
 }
