7d4ab51672eeaa5f60e8aaf6cc8062ba0247fde6
Matthew Price <hello@mattprice.me>
2013-07-17 23:40:41 -0400

    Use strict output comparisons in the unit tests.

--- apns_test.go
+++ apns_test.go
@@ -5,66 +5,104 @@ import (
 )
 
 func Test_Notification_ToJSON(t *testing.T) {
-	// Test a basic notification.
-	payload := NewNotification()
-	payload.Alert = "Hello, World! This is a test."
+	// Test an empty alert.
+	{
+		payload := NewNotification()
 
-	bytes, err := payload.ToJSON()
-	if err != nil {
-		t.Error("Unexpected error:", err)
+		bytes, err := payload.ToJSON()
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
+
+		expected := `{"aps":{}}`
+		if received := string(bytes); expected != received {
+			t.Errorf("Expected %#q but found %#q.", expected, received)
+		}
 	}
 
-	if len(bytes) != 49 {
-		t.Errorf("Converting basic notification to JSON failed. Expected 49 bytes but found %v.", len(bytes))
+	// Test basic alerts.
+	{
+		payload := NewNotification()
+		payload.Alert = "Hello, World!"
+
+		bytes, err := payload.ToJSON()
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
+
+		expected := `{"aps":{"alert":"Hello, World!"}}`
+		if received := string(bytes); expected != received {
+			t.Errorf("Expected %#q but found %#q.", expected, received)
+		}
 	}
 
-	// Test a complex notification.
-	payload = NewNotification()
-	payload.Alert = "Hello, World! This is a test."
-	payload.Badge = 42
-	payload.Sound = "Test.aiff"
-	payload.LaunchImage = "Default.png"
-	payload.Custom["Engines"] = Custom{
-		"DDG":  "https://duckduckgo.com/",
-		"Bing": "https://bing.com/",
-	}
+	// Test the alert dictionary format.
+	{
+		payload := NewNotification()
+		payload.Alert = "Hello, World!"
+		payload.Sound = "Test.aiff"
+		payload.LaunchImage = "Default.png"
 
-	bytes, err = payload.ToJSON()
-	if err != nil {
-		t.Error("Unexpected error:", err)
-	}
+		bytes, err := payload.ToJSON()
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
 
-	if len(bytes) != 189 {
-		t.Errorf("Converting complex notification to JSON failed. Expected 189 bytes but found %v.", len(bytes))
+		expected := `{"aps":{"alert":{"body":"Hello, World!","launch-image":"Default.png"},"sound":"Test.aiff"}}`
+		if received := string(bytes); expected != received {
+			t.Errorf("Expected %#q but found %#q.", expected, received)
+		}
 	}
 
 	// Test a localized notification.
 	// Conveniently, this also tests setting no badge count.
-	payload = NewNotification()
-	payload.Alert = "This should be ignored."
-	payload.AlertLocKey = "APP_GREETING_KEY"
-	payload.AlertLocArgs = []string{"John", "Smith"}
+	{
+		payload := NewNotification()
+		payload.Alert = "This should be ignored."
+		payload.AlertLocKey = "APP_GREETING_KEY"
+		payload.AlertLocArgs = []string{"John", "Smith"}
 
-	bytes, err = payload.ToJSON()
-	if err != nil {
-		t.Error("Unexpected error:", err)
-	}
+		bytes, err := payload.ToJSON()
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
 
-	if len(bytes) != 76 {
-		t.Errorf("Converting localized notification to JSON failed. Expected 76 bytes but found %v.", len(bytes))
+		expected := `{"aps":{"alert":{"loc-args":["John","Smith"],"loc-key":"APP_GREETING_KEY"}}}`
+		if received := string(bytes); expected != received {
+			t.Errorf("Expected %#q but found %#q.", expected, received)
+		}
 	}
 
 	// Test a badge count of 0.
 	// This was a problem in the past when using other `json.Marshal` methods.
-	payload = NewNotification()
-	payload.Badge = 0
+	{
+		payload := NewNotification()
+		payload.Badge = 0
 
-	bytes, err = payload.ToJSON()
-	if err != nil {
-		t.Error("Unexpected error:", err)
+		bytes, err := payload.ToJSON()
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
+
+		expected := `{"aps":{"badge":0}}`
+		if received := string(bytes); expected != received {
+			t.Errorf("Expected %#q but found %#q.", expected, received)
+		}
 	}
 
-	if len(bytes) != 19 {
-		t.Errorf("Alert with badge count of 0 failed. Expected 19 bytes but found %v.", len(bytes))
+	// Test a badge count of 42.
+	{
+		payload := NewNotification()
+		payload.Badge = 42
+
+		bytes, err := payload.ToJSON()
+		if err != nil {
+			t.Error("Unexpected error:", err)
+		}
+
+		expected := `{"aps":{"badge":42}}`
+		if received := string(bytes); expected != received {
+			t.Errorf("Expected %#q but found %#q.", expected, received)
+		}
 	}
 }
