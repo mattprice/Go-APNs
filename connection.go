b423f32c01cd563f1f9b91ec61de44d9978ee737
Matthew Price <hello@mattprice.me>
2013-07-25 23:03:55 -0400

    Add support for production connections.

--- connection.go
+++ connection.go
@@ -6,18 +6,35 @@ import (
 	"strings"
 )
 
+const (
+	productionAddress = "gateway.push.apple.com:2195"
+	sandboxAddress    = "gateway.sandbox.push.apple.com:2195"
+)
+
 var (
 	gatewayConnection *tls.Conn
+	gatewayAddress    string
 	connectionCert    tls.Certificate
 )
 
-func NewSandboxConnection(cert, key string) (err error) {
+func NewConnection(cert, key string) error {
+	gatewayAddress = productionAddress
+	return connection(cert, key)
+}
+
+func NewSandboxConnection(cert, key string) error {
+	gatewayAddress = sandboxAddress
+	return connection(cert, key)
+}
+
+func connection(cert, key string) (err error) {
 	if strings.HasSuffix(cert, ".pem") || strings.HasSuffix(key, ".pem") {
+		// Load the certificate from files.
 		connectionCert, err = tls.LoadX509KeyPair(cert, key)
 	} else {
+		// Load the certificate from input strings.
 		connectionCert, err = tls.X509KeyPair([]byte(cert), []byte(key))
 	}
-
 	if err != nil {
 		return err
 	}
@@ -26,12 +43,13 @@ func NewSandboxConnection(cert, key string) (err error) {
 		Certificates: []tls.Certificate{connectionCert},
 	}
 
-	conn, err := net.Dial("tcp", "gateway.sandbox.push.apple.com:2195")
+	conn, err := net.Dial("tcp", gatewayAddress)
 	if err != nil {
 		return err
 	}
 
-	// TODO: Only one variable for all connections? Doesn't seem smart.
+	// TODO: Connection information probably needs to be handled better to support
+	// creating production and sandbox connections at the same time.
 	gatewayConnection = tls.Client(conn, conf)
 	err = gatewayConnection.Handshake()
 	if err != nil {
